93fe88ff7be92a0adab07ab063be9196ff70db8a
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-11-02 21:29:08 +0100

    Added Controller Support and pollished keyboard Controll
    - added Controller Support for 2 Players
    - changed keyboard Input algorythm so conflicting input gets neutrulized
    - added Player-Scene

--- World/Player.gd
+++ World/Player.gd
@@ -11,13 +11,14 @@ func _process(delta):
 
 func get_movement(delta):
 	var input = Vector2()
+	
 	if Input.is_action_pressed("player1_up"):
-		input.y = -1
+		input.y -= 1
 	if Input.is_action_pressed("player1_down"):
-		input.y = 1
+		input.y += 1
 	if Input.is_action_pressed("player1_left"):
-		input.x = -1
+		input.x -= 1
 	if Input.is_action_pressed("player1_right"):
-		input.x = 1
+		input.x += 1
 	
 	move_and_collide(input * delta * movementSpeed)
