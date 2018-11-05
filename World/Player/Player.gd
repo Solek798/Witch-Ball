df54f1582eeafaebbfa31b6e5cbd940522abb0f1
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-11-05 01:08:06 +0100

    Adjusted Player Controll to instance-mechanic
    - adjusted Player Controll
    - fixed defective Controller Inputmap for Player 2

--- World/Player/Player.gd
+++ World/Player/Player.gd
@@ -2,9 +2,16 @@ extends KinematicBody2D
 
 export(int) var movementSpeed 
 
+enum action {UP, DOWN, LEFT, RIGHT}
 
+# template for action event Strings
+var ctr = ["player%d_up", "player%d_down", "player%d_left", "player%d_right"]
+var player_id
+
+# formating action event Strings for this Player
 func _ready():
-	pass
+	for i in range(ctr.size()):
+		ctr[i] = ctr[i] % player_id
 
 func _process(delta):
 	get_movement(delta)
@@ -12,13 +19,13 @@ func _process(delta):
 func get_movement(delta):
 	var input = Vector2()
 	
-	if Input.is_action_pressed("player1_up"):
+	if Input.is_action_pressed(ctr[UP]):
 		input.y -= 1
-	if Input.is_action_pressed("player1_down"):
+	if Input.is_action_pressed(ctr[DOWN]):
 		input.y += 1
-	if Input.is_action_pressed("player1_left"):
+	if Input.is_action_pressed(ctr[LEFT]):
 		input.x -= 1
-	if Input.is_action_pressed("player1_right"):
+	if Input.is_action_pressed(ctr[RIGHT]):
 		input.x += 1
 	
 	move_and_collide(input * delta * movementSpeed)
--- World/World.gd
+++ World/World.gd
@@ -8,5 +8,10 @@ onready var players = []
 
 func _ready():
 	players.append(player_scene.instance())
+	players.back().player_id = players.size()
+	add_child(players.back())
+	
+	players.append(player_scene.instance())
+	players.back().player_id = players.size()
 	add_child(players.back())
 
