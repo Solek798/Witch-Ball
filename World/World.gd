0053e8d655abe1898b979fb3d71bb8a1504cea94
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-11-09 00:40:39 +0100

    Minor changes in vafor of development
    - renamed Animation_Player1 to Animation_Player
    - renamed Images_Player1 to Images_Player
    - exposed position and scale of both Players to the inspector
    - added AnimationPlayerTree-Scene as AnimationPlayerTree in Player-Scene
    - moved AnimationPlayer from Player-Scene to AnimationPlayerTree-Scene

--- World/World.gd
+++ World/World.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 export(PackedScene) var player_scene
+export(Vector2) var player1_position
+export(Vector2) var player2_position
+export(Vector2) var player1_scale
+export(Vector2) var player2_scale
 
 signal player_created
 
@@ -8,13 +12,18 @@ onready var players = []
 
 func _ready():
 	players.append(player_scene.instance())
+	players.back().position = player1_position
+	players.back().scale = player1_scale
 	players.back().player_id = players.size()
 	players.back().connect("projectile_thrown", self, "_on_projectile_thrown")
 	add_child(players.back())
 	
-	#players.append(player_scene.instance())
-	#players.back().player_id = players.size()
-	#add_child(players.back())
+	players.append(player_scene.instance())
+	players.back().position = player2_position
+	players.back().scale = player2_scale
+	players.back().player_id = players.size()
+	players.back().connect("projectile_thrown", self, "_on_projectile_thrown")
+	add_child(players.back())
 
 func _on_projectile_thrown(node):
 	add_child(node) 
