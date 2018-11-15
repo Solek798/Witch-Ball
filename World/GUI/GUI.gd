4ae37e06aa6660012b71994c19489173b47d8f9f
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-11-15 21:34:23 +0100

    Player health bound to the ingame UI
    - player health is now editable in the inspector

--- World/GUI/GUI.gd
+++ World/GUI/GUI.gd
@@ -1,12 +1,10 @@
 extends Container
 
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+export(PackedScene) var life_template
 
 func _on_player_created(player):
-	pass
+	for i in range(player.health):
+		var new_life = life_template.instance()
+		get_node("Healthbars/Player%dHealthbar" % player.player_id).add_child(new_life)
 
 
