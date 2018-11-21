a7132264ffd8ea742d103c2df527e30e37e3428b
Antonio Waletzki <Antonio Waletzki@localhost>
2018-11-21 17:26:22 +0100

    fixed Merge Problems
    - solved naming problems
    - (re)added test background
    - (re)implemented dodge feature in PlayerController
    - (re)added InputMap for dodge feature

--- World/Player/PlayerController.gd
+++ World/Player/PlayerController.gd
@@ -7,20 +7,33 @@ const FORMAT_STRINGS = [
 	"player%d_right",
 	"player%d_aim_up",
 	"player%d_aim_down",
-	"player%d_throw"
+	"player%d_throw",
+	"player%d_dodge"
 ]
 
-var keys = []
+onready var keys = []
+onready var locked_states = []
 
 func setup(id):
-	keys.clear()
-	
 	for string in FORMAT_STRINGS:
 		keys.append(string % id)
 
 func state(action):
-	if action == 6:
+	if locked_states.size() > 0:
+		return locked_states[action]
+	
+	if action > 5:
 		return Input.is_action_just_pressed(keys[action])
 	else:
 		return Input.is_action_pressed(keys[action])
 	
+
+func lock():
+	for key in keys:
+		locked_states.append(Input.is_action_pressed(key))
+
+func unlock():
+	locked_states.clear()
+
+func _on_DodgeTimer_timeout():
+	unlock()
