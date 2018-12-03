0fdc47bdfda11f8a38fd6925a2c5faf2a4046f60
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-12-03 12:46:14 +0100

    Dismantled GUI and Arena Scene to additional extra Scenes
    - added Classic.tscn
    - added Position_Classic.tscn
    - added Backstage_Classic.tscn
    - added Healthbar_Classic.tscn
    - added Info_Classic.tscn

--- Match_Type/MatchType.gd
+++ Match_Type/MatchType.gd
@@ -1,3 +1,34 @@
 extends Node
 
+export(PackedScene) var arena
+export(PackedScene) var gui
+export(PackedScene) var backstage
+export(PackedScene) var player_scene
+export(int) var player_count
 
+onready var players = []
+
+var arena
+var gui
+var backstage
+
+
+func _ready():
+	# creates the in player_count specified ammount of players
+	for i in player_count:
+		players.append(create_player(players.size() + 1))
+
+func create_player(id):
+	#instanciates and sets th player to the specified position
+	var player = player_scene.instance()
+	player.position = get_node("PositionPlayer%d" % id).position
+	player.id = id
+	# connects all player relevant signals
+	player.connect("bullet_thrown", self, "_on_bullet_thrown")
+	player.connect("player_created", $GUI, "_on_player_created")
+	player.connect("player_damaged", $GUI, "_on_player_damaged")
+	player.connect("player_dead", self, "_on_player_dead")
+	player.connect("player_reset", $GUI, "_on_player_reset")
+	# adds the player to the world
+	add_child(player)
+	return player
