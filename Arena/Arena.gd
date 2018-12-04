5b59da33a60e84d54c00765a5881bf0be130b369
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-12-04 11:21:31 +0100

    fixed bug with player scale

--- Arena/Arena.gd
+++ Arena/Arena.gd
@@ -6,6 +6,9 @@ func _on_bullet_thrown(bullet):
 
 func _on_player_created(player):
 	player.position = $Position.get_by_id(player.id)
+	# TEMP
+	if player.id == 2:
+		player.scale = Vector2(-1, 1)
 	add_child(player)
 
 func _on_player_reseted(player):
