99c748b39eac4af14a9b427f12d9ce9e81d40da6
fkonprecht <39970051+Solek798@users.noreply.github.com>
2018-12-07 12:48:45 +0100

    fixed visial bug with player 2 info win counter

--- GUI/Info/Classic/Info_Classic.gd
+++ GUI/Info/Classic/Info_Classic.gd
@@ -6,9 +6,15 @@ export(PackedScene) var round_template
 
 func set_winner(id):
 	var children = get_node("Player%dwins/Player%dRounds" % [id, id]).get_children()
+	
 	for i in children.size():
+		if id == 2:
+			i = -i - 1
+		print(i)
 		if not children[i].is_filled:
 			children[i].is_filled = true
+			if id == 2:
+				return i == -children.size()
 			return i == children.size() - 1
 
 func set_rounds(ammount):
