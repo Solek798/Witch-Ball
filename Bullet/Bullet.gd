6e30aaa600de84d30570531a26d277674424f1cb
fkonprecht <39970051+Solek798@users.noreply.github.com>
2019-01-10 17:32:18 +0100

    Added Sound Effects for throwing and round end

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -3,12 +3,13 @@ extends RigidBody2D
 export(int) var speed
 export(int) var damage
 
-signal bullet_destroyed()
+signal bullet_destroyed(sound_effect)
 
 var own_player
 
 func _ready():
 	add_collision_exception_with(own_player)
+	$Shot.play()
 
 func _process(delta):
 	# scans for collisions
@@ -18,5 +19,11 @@ func _process(delta):
 		# if the collider is a player, he takes the specified ammount of damage
 		if collision.front().has_method("take_damage"):
 			collision.front().take_damage(damage)
+		
+		var sound_effect = $Collide
+		remove_child(sound_effect)
+		sound_effect.global_position = self.global_position
+		sound_effect.play()
+		emit_signal("bullet_destroyed", sound_effect)
 		queue_free()
 		#emit_signal("bullet_destroyed")
