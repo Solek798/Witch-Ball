c329a78d2962ffb644168a8669504a3ab654125d
fkonprecht <39970051+Solek798@users.noreply.github.com>
2019-01-11 12:35:00 +0100

    big bullet gets destroyed by smal bullet fix

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -37,6 +37,6 @@ func _physics_process(delta):
 func _on_Area2D_body_entered(body):
 	if body == self:
 		return
-	
+	print(body)
 	if body.is_in_group("Bullet") and not body.name.matchn("*Bullet_Big*"):
 		body.queue_free()
