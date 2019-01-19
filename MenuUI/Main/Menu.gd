8592e9292915891d862d13ac18062be4c4fd4591
Antonio Waletzki <Antonio Waletzki@localhost>
2019-01-19 15:54:29 +0100

    add Particle Menue

--- MenuUI/Main/Menu.gd
+++ MenuUI/Main/Menu.gd
@@ -4,9 +4,12 @@ var selection  #Start
 var selection1 #Option
 var Credits #Credits
 
+
 func _on_start_pressed():
 	selection.visible = true
 	self.visible = false
+	
+	
 	
 func _on_options_pressed():
 	selection1.visible = true
@@ -17,11 +20,7 @@ func _on_options_pressed():
 func _on_Credits_pressed():
 	Credits.visible = true
 	self.visible = false
-	
-	
-	pass 
-	
-	
+
 	
 func _on_quit_pressed():
 	get_tree().quit()
