a7b72fa6a3f17801d1bada1ff9f5ec1b9c8d5e18
Antonio Waletzki <Antonio Waletzki@localhost>
2019-01-19 13:36:49 +0100

    Menu verlinkung

--- MenuUI/Main/Menu.gd
+++ MenuUI/Main/Menu.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
-var selection
-
+var selection  #Start
+var selection1 #Option
+var Credits #Credits
 
 func _on_start_pressed():
 	selection.visible = true
 	self.visible = false
-
+	
+func _on_options_pressed():
+	selection1.visible = true
+	self.visible = false
+	
+	
+	
+func _on_Credits_pressed():
+	Credits.visible = true
+	self.visible = false
+	
+	
+	pass 
+	
+	
+	
 func _on_quit_pressed():
 	get_tree().quit()
+
