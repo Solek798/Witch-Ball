55e59ef97a04149a8497e92b15997b94592a351f
Ronja Brodocz <Ronja Brodocz@localhost>
2019-01-22 18:30:48 +0100

    replaced and adjusted GUI
    - Changed Code behavoir

--- GUI/Info/Classic/Info_Classic.gd
+++ GUI/Info/Classic/Info_Classic.gd
@@ -6,14 +6,11 @@ export(Texture) var round_won_template
 
 
 func set_winner(id):
-	var children = get_node("Player%dwins/Player%dRounds" % [id, id]).get_children()
+	var children = get_node("Railing/Player%d" % id).get_children()
 	
-	for i in children.size():
-		if id == 2:
-			i = -i - 1
-		print(i)
-		if not children[i].is_filled:
-			children[i].is_filled = true
-			if id == 2:
-				return i == -children.size()
-			return i == children.size() - 1
+	for child in children:
+		if not child.is_filled:
+			child.is_filled = true
+			break
+	
+	return get_node("Railing/Player%d/Round3" % id).is_filled
