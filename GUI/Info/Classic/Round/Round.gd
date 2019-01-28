c05eb4606843ea4f0d1d1163557edd136f1b524d
Antonio Waletzki <Antonio Waletzki@localhost>
2019-01-28 09:04:04 +0100

    Update organisation Bullet/effect resources

--- GUI/Info/Classic/Round/Round.gd
+++ GUI/Info/Classic/Round/Round.gd
@@ -8,6 +8,9 @@ var is_filled = false setget set_filled
 func set_filled(fill):
 	if fill:
 		self.texture = round_filled
+		$AnimationTreePlayer.oneshot_node_start("oneshot_Round")
+		
 	else:
 		self.texture = round_empty
 	is_filled = fill
+
