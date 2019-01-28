75eac193cb221289bd00f41e5a6c3019b1538d75
Antonio Waletzki <Antonio Waletzki@localhost>
2019-01-28 15:29:35 +0100

    Update GUI

--- GUI/Info/Classic/Round/Round.gd
+++ GUI/Info/Classic/Round/Round.gd
@@ -7,10 +7,11 @@ var is_filled = false setget set_filled
 
 func set_filled(fill):
 	if fill:
-		self.texture = round_filled
 		$AnimationTreePlayer.oneshot_node_start("oneshot_Round")
+		self.texture = round_filled
 		
 	else:
 		self.texture = round_empty
 	is_filled = fill
-
+	
+	
