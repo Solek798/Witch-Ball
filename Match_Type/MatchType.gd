91dce36b95aa80c2ce231311c9b6d349c77677e7
fkonprecht <39970051+Solek798@users.noreply.github.com>
2019-02-06 17:10:16 +0100

    added Tutorial Scene

--- Match_Type/MatchType.gd
+++ Match_Type/MatchType.gd
@@ -57,13 +57,7 @@ func create_player(id):
 	player.connect("player_reseted", arena, "_on_player_reseted")
 	player.connect("player_reseted", backstage, "_on_player_reseted")
 	backstage.connect("round_finished", player, "_on_round_finished")
-	
-	
-	
 	backstage.connect("player_won_round", player, "_on_player_won_round" )
-	
-	
-	
 	
 	emit_signal("player_created", player)
 
