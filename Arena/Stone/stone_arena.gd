52ef38265ac0ef92f8677a75e193defb9b13fe38
fkonprecht <39970051+Solek798@users.noreply.github.com>
2019-02-20 12:10:06 +0100

    Changed Arena scenes to Inhereted Sccene

---
+++ Arena/Stone/stone_arena.gd
@@ -0,0 +1,47 @@
+extends "res://Arena/arena.gd"
+
+export(PackedScene) var stone_effect_template
+export(PackedScene) var needle_effect_template
+export(PackedScene) var eye_effect_template
+export(PackedScene) var butterfly_effect_template
+export(int) var min_effect_time = 6
+export(int) var max_effect_time = 10
+
+
+func _ready():
+	set_effect_time()
+	$Timer/RandomEffect.start()
+
+func set_effect_time():
+	$Timer/RandomEffect.wait_time = (randi() % (max_effect_time - min_effect_time)) + min_effect_time
+
+func _on_Stones_body_entered(body):
+	if body is RigidBody2D:
+		var anim = stone_effect_template.instance()
+		anim.global_position = body.global_position
+		anim.emitting = true
+		add_child(anim)
+		body.destroy()
+
+func _on_Needles_body_entered(body):
+	if body is RigidBody2D:
+		var anim = needle_effect_template.instance()
+		anim.global_position = body.global_position
+		anim.emitting = true
+		add_child(anim)
+
+func _on_RandomEffect_timeout():
+	var children
+	var effect
+	
+	if randi() % 2:
+		children = $Eyes.get_children()
+		effect = eye_effect_template.instance()
+	else:
+		children = $Butterflies.get_children()
+		effect = butterfly_effect_template.instance()
+	
+	effect.position = children[randi() % children.size()].position
+	effect.start()
+	
+	add_child(effect)
--- Bullet/Big/big_bullet.gd
+++ Bullet/Big/big_bullet.gd
@@ -2,5 +2,5 @@ extends "res://Bullet/bullet.gd"
 
 
 func _on_BulletDetection_body_entered(body):
-	if body != self and not body.has_method("_on_BulletDetection_body_entered"):
+	if body is RigidBody and not body.has_method("_on_BulletDetection_body_entered"):
 		body.queue_free()
