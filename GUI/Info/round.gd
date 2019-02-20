5933c3af815b8dc94d70868e9762d4b263380d04
Antonio Waletzki <Antonio Waletzki@localhost>
2019-02-20 19:00:16 +0100

    Animation Throw

--- GUI/Info/round.gd
+++ GUI/Info/round.gd
@@ -7,11 +7,14 @@ var is_filled = false setget set_filled
 
 func set_filled(fill):
 	if fill:
-		$AnimationTreePlayer.oneshot_node_start("oneshot_Round")
-		self.texture = round_filled
+		$AnimationPlayer.play("Gewinn")
 		
 	else:
 		self.texture = round_empty
 	is_filled = fill
 	
+	
+	
+func RoundIsFilled():
+	self.texture = round_filled
 	
