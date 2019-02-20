e8ed0e1ee9eaaed93fb911bde67d2e869d0682d1
Antonio Waletzki <Antonio Waletzki@localhost>
2019-02-20 13:56:27 +0100

    add variationen of life Animation

--- GUI/Healthbar/life.gd
+++ GUI/Healthbar/life.gd
@@ -1,6 +1,8 @@
 extends TextureRect
 func AnimationHeart():
 	$AnimationPlayer.play("HeartFadeOut")
+	#$AnimationPlayer.play("HeartFadeOut1")
+	#$AnimationPlayer.play("HeartFadeOut2")
 
 func HeartIsEmpty():
 	queue_free()
