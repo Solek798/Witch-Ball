dadcd0a843d71e4cf5cd24a334494362d98a6f4f
fkonprecht <39970051+Solek798@users.noreply.github.com>
2019-02-20 18:13:33 +0100

    further work on bullets
    known bugs:
    big_bullets get destroyed by other bullets

--- Bullet/Big/big_bullet.gd
+++ Bullet/Big/big_bullet.gd
@@ -2,5 +2,12 @@ extends "res://Bullet/bullet.gd"
 
 
 func _on_BulletDetection_body_entered(body):
-	if body is RigidBody and not body.has_method("_on_BulletDetection_body_entered"):
+	if body == self:
+		return
+	# TODO !!!!!
+	#print(body)
+	#print(body is RigidBody2D)
+	#print(body.has_method("_on_BulletDetection_body_entered"))
+	#if body is RigidBody2D and not body.has_method("_on_BulletDetection_body_entered"):
+	if body.is_in_group("Bullet") and not body.name.matchn("*BigBullet*"):
 		body.queue_free()
