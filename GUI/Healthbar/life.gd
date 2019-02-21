83116dea60b52802e758e908c0e2fcb8ee0acb7e
fkonprecht <39970051+Solek798@users.noreply.github.com>
2019-02-21 02:10:53 +0100

    Fixed heart Animation

--- GUI/Healthbar/life.gd
+++ GUI/Healthbar/life.gd
@@ -1,8 +1,15 @@
 extends TextureRect
-func AnimationHeart():
-	$AnimationPlayer.play("HeartFadeOut")
-	#$AnimationPlayer.play("HeartFadeOut1")
-	#$AnimationPlayer.play("HeartFadeOut2")
 
-func HeartIsEmpty():
-	queue_free()
+onready var is_filled = true setget set_is_filled
+
+
+func set_is_filled(fill):
+	if fill:
+		$Animation.stop()
+		$Animation.play("Reset")
+	else:
+		$Animation.play("HeartFadeOut")
+		#$Animation.play("HeartFadeOut1")
+		#$Animation.play("HeartFadeOut2")
+	
+	is_filled = fill
