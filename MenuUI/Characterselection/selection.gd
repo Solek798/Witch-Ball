f2ed568484341576e93b81af6b2fce2609aee3d1
Antonio Waletzki <Antonio Waletzki@localhost>
2019-03-04 17:07:02 +0100

    Update and final CharacterSelectionScene

--- MenuUI/Characterselection/selection.gd
+++ MenuUI/Characterselection/selection.gd
@@ -7,16 +7,20 @@ onready var player_2 = false setget set_player_2
 func set_player_1(new_value):
 	player_1 = new_value
 	if new_value:
-		$Player1.text = "P1"
+		$Player1Selected.visible = true
+		$Node2D/Player1.text = "P1"
 	else:
-		$Player1.text = ""
+		$Node2D/Player1.text = ""
+		$Player1Selected.visible = false
 
 func set_player_2(new_value):
 	player_2 = new_value
 	if new_value:
-		$Player2.text = "P2"
+		$Player2Selected.visible = true
+		$Node2D/Player2.text = "P2"
 	else:
 		$Player2.text = ""
+		$Node2D/Player2.text = false
 
 func reset():
 	set_player_1(false)
