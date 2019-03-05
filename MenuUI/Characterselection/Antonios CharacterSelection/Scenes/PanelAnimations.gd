33f42560505cbd9b0e40177b77e19d2d74d177fd
Antonio Waletzki <Antonio Waletzki@localhost>
2019-03-05 22:48:33 +0100

    Hover PlayerColor how can select

--- MenuUI/Characterselection/Antonios CharacterSelection/Scenes/PanelAnimations.gd
+++ MenuUI/Characterselection/Antonios CharacterSelection/Scenes/PanelAnimations.gd
@@ -4,8 +4,11 @@ extends Node2D
 
 func _on_Panel_focus_entered():
 	$AnimationPlayer.play("AntoniosCharacterselection")
+	$Selections.visible = true
+	
 
 
 func _on_Panel_focus_exited():
 	$AnimationPlayer.play_backwards("AntoniosCharacterselection")
+	$Selections.visible = false
 
